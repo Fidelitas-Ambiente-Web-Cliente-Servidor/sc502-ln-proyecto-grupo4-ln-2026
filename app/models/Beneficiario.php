e236e2f278e1be5ec938bf2bcaa6d43e5c259108
DeibyVargasChaves <dvargas30884@ufide.ac.cr>
2026-04-18 17:25:33 -0600

    Reservas y Perfil de beneficiario
    Paneles listos de reservas y perfil

--- app/models/Beneficiario.php
+++ app/models/Beneficiario.php
@@ -1,12 +1,15 @@
 <?php
-class Beneficiario {
+class Beneficiario
+{
     private $conn;
 
-    public function __construct($db) {
+    public function __construct($db)
+    {
         $this->conn = $db;
     }
 
-    public function crear($id_usuario, $datos) {
+    public function crear($id_usuario, $datos)
+    {
         $query = "INSERT INTO beneficiarioProyecto
                     (id_usuario, nombre_completo, cedula_identidad, telefono,
                      provincia, canton, direccion, fecha_nacimiento)
@@ -27,12 +30,44 @@ class Beneficiario {
         return $stmt->affected_rows > 0;
     }
 
-    public function getByIdUsuario($id_usuario) {
+    public function getByIdUsuario($id_usuario)
+    {
         $query = "SELECT * FROM beneficiarioProyecto WHERE id_usuario = ?";
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param("i", $id_usuario);
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_assoc();
-}
+    }
+
+    public function actualizar($id_beneficiario, $datos)
+    {
+        $query = "UPDATE beneficiarioProyecto 
+              SET nombre_completo = ?, cedula_identidad = ?, telefono = ?,
+                  provincia = ?, canton = ?, direccion = ?, fecha_nacimiento = ?
+              WHERE id_beneficiario = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param(
+            "sssssssi",
+            $datos['nombre_completo'],
+            $datos['cedula_identidad'],
+            $datos['telefono'],
+            $datos['provincia'],
+            $datos['canton'],
+            $datos['direccion'],
+            $datos['fecha_nacimiento'],
+            $id_beneficiario
+        );
+        $stmt->execute();
+        return $stmt->affected_rows > 0;
+    }
+
+    public function eliminar($id_beneficiario)
+    {
+        $query = "DELETE FROM beneficiarioProyecto WHERE id_beneficiario = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("i", $id_beneficiario);
+        $stmt->execute();
+        return $stmt->affected_rows > 0;
+    }
 }
